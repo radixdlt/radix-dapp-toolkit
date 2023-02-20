f7253cfe34f0a2ba8404056346f963d5291a52ee
Alex Stelea <alex.s@rdx.works>
2023-02-20 16:00:47 +0100

    fix: remove use persona in one time request

--- src/state/helpers/with-auth.ts
+++ src/state/helpers/with-auth.ts
@@ -1,6 +1,7 @@
 import { DataRequestValue, State } from '../../_types'
 
 export const withAuth = (request: DataRequestValue, state: State) => {
+  if (request.oneTimeAccountsWithoutProofOfOwnership) return request
   if (state.persona)
     return {
       ...request,
