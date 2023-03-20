1cd3a6fecfb63eb52cb731ee194091e817913410
Alex Stelea <alex.s@rdx.works>
2023-03-20 10:07:16 +0000

    feat: show notification icon when request item changed

--- src/connect-button/subjects.ts
+++ src/connect-button/subjects.ts
@@ -7,11 +7,12 @@ export const ConnectButtonSubjects = () => ({
   onDisconnect: new Subject<void>(),
   onUpdateSharedData: new Subject<void>(),
   loading: new BehaviorSubject<boolean>(false),
-  connected: new ReplaySubject<boolean>(),
+  connected: new ReplaySubject<boolean>(1),
   requestItems: new BehaviorSubject<RequestItem[]>([]),
   onCancelRequestItem: new Subject<string>(),
   accounts: new BehaviorSubject<Account[]>([]),
   connecting: new BehaviorSubject(false),
+  onShowPopover: new Subject<void>(),
   showNotification: new BehaviorSubject(false),
   personaLabel: new BehaviorSubject<string>(''),
 })
