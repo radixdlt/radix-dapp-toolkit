5895b4ec3d96d37c64cc5cb12ecc5931fe2c3251
Dawid Sowa <dawid.sowa@rdx.works>
2023-05-03 16:41:51 +0200

    feat: bump @radixdlt/babylon-gateway-api-sdk

--- src/gateway/gateway-api.ts
+++ src/gateway/gateway-api.ts
@@ -1,96 +1,32 @@
-import {
-  TransactionApi,
-  Configuration,
-  StateApi,
-  StateEntityDetailsResponseItem,
-  FungibleResourcesCollection,
-  FungibleResourcesCollectionItemVaultAggregated,
-  NonFungibleResourcesCollection,
-  NonFungibleResourcesCollectionItemVaultAggregated,
-  StateEntityDetailsResponse,
-} from '@radixdlt/babylon-gateway-api-sdk'
+import { GatewayApiClient as BabylonGatewayApiClient } from '@radixdlt/babylon-gateway-api-sdk'
 import { ResultAsync } from 'neverthrow'
 import { errorIdentity } from '../helpers/error-identity'
 
 export type GatewayApiClient = ReturnType<typeof GatewayApiClient>
 
-export type FungibleResourcesVaultCollection = Omit<
-  FungibleResourcesCollection,
-  'items'
-> & {
-  items: FungibleResourcesCollectionItemVaultAggregated[]
-}
-
-export type NonFungibleResourcesVaultCollection = Omit<
-  NonFungibleResourcesCollection,
-  'items'
-> & {
-  items: NonFungibleResourcesCollectionItemVaultAggregated[]
-}
-
-export type StateEntityDetailsVaultResponseItem =
-  StateEntityDetailsResponseItem & {
-    fungible_resources: FungibleResourcesVaultCollection
-    non_fungible_resources: NonFungibleResourcesVaultCollection
-  }
-
 export const GatewayApiClient = (basePath: string) => {
-  const configuration = new Configuration({ basePath })
-  const transactionApi = new TransactionApi(configuration)
-  const stateApi = new StateApi(configuration)
+  const { transaction, state } = BabylonGatewayApiClient.initialize({
+    basePath,
+  })
 
   const getTransactionStatus = (transactionIntentHashHex: string) =>
     ResultAsync.fromPromise(
-      transactionApi.transactionStatus({
-        transactionStatusRequest: {
-          intent_hash_hex: transactionIntentHashHex,
-        },
-      }),
+      transaction.getStatus(transactionIntentHashHex),
       errorIdentity
     )
 
-  const getTransactionDetails = (
-    transactionIntentHashHex: string,
-    stateVersion?: number
-  ) =>
+  const getTransactionDetails = (transactionIntentHashHex: string) =>
     ResultAsync.fromPromise(
-      transactionApi.transactionCommittedDetails({
-        transactionCommittedDetailsRequest: {
-          intent_hash_hex: transactionIntentHashHex,
-          ...(stateVersion
-            ? {
-                at_ledger_state: {
-                  state_version: stateVersion,
-                },
-              }
-            : {}),
-        },
-      }),
+      transaction.getCommittedDetails(transactionIntentHashHex),
       errorIdentity
     )
 
   const getEntityDetails = (address: string) =>
-    ResultAsync.fromPromise(
-      stateApi
-        .stateEntityDetails({
-          stateEntityDetailsRequest: {
-            addresses: [address],
-            aggregation_level: 'Vault',
-          },
-        })
-        .then(({ items }) => items[0] as StateEntityDetailsVaultResponseItem),
-      errorIdentity
-    )
+    ResultAsync.fromPromise(state.getEntityDetailsVaultAggregated(address), errorIdentity)
 
   const getEntitiesDetails = (addresses: string[]) =>
     ResultAsync.fromPromise(
-      stateApi.stateEntityDetails({
-        stateEntityDetailsRequest: { addresses, aggregation_level: 'Vault' },
-      }) as Promise<
-        StateEntityDetailsResponse & {
-          items: StateEntityDetailsVaultResponseItem[]
-        }
-      >,
+      state.getEntityDetailsVaultAggregated(addresses),
       errorIdentity
     )
 
@@ -104,7 +40,7 @@ export const GatewayApiClient = (basePath: string) => {
     vaultAddress: string
   }) =>
     ResultAsync.fromPromise(
-      stateApi.entityNonFungibleIdsPage({
+      state.innerClient.entityNonFungibleIdsPage({
         stateEntityNonFungibleIdsPageRequest: {
           address: accountAddress,
           vault_address: vaultAddress,
@@ -120,7 +56,5 @@ export const GatewayApiClient = (basePath: string) => {
     getEntityDetails,
     getEntitiesDetails,
     getEntityNonFungibleIds,
-    transactionApi,
-    stateApi,
   }
 }
