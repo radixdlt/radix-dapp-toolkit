87dad6d284f77bc961c151f9fb133c77dc856a58
Alex Stelea <alex.s@rdx.works>
2023-05-18 12:45:53 +0100

    refactor: update connect button request data type

--- src/_types.ts
+++ src/_types.ts
@@ -1,15 +1,9 @@
 import { ResultAsync } from 'neverthrow'
 import { WalletSdk, Account, PersonaData } from '@radixdlt/wallet-sdk'
-import { SdkError } from '@radixdlt/wallet-sdk/dist/helpers/error'
 import { Observable } from 'rxjs'
 import { WalletClient } from './wallet/wallet-client'
 import { RequestItem } from '@radixdlt/connect-button'
 import { GatewayClient } from './gateway/gateway'
-import {
-  ConnectButtonDataRequestInput,
-  DataRequestInput,
-  RdtStateWalletData,
-} from './io/schemas'
 import { StateClient } from './state/state'
 import { RequestItemClient } from './request-items/request-item-client'
 
@@ -42,14 +36,3 @@ export type Providers = {
   requestItemClient: RequestItemClient
   storageClient: StorageProvider
 }
-
-export type RequestData = (
-  value: DataRequestInput,
-  done?: () => void
-) => ResultAsync<RdtStateWalletData, SdkError>
-
-export type OnConnect = (
-  value: (
-    value: ConnectButtonDataRequestInput
-  ) => ResultAsync<RdtStateWalletData, SdkError>
-) => void | boolean | Promise<void | boolean>
