37d99684a8735f00e5e27d9a3531ad0a43895ed3
Dawid Sowa <dawid.sowa@rdx.works>
2023-07-19 16:04:28 +0200

    feat: create network map from predefined list

--- src/gateway/_types.ts
+++ src/gateway/_types.ts
@@ -1,27 +1,8 @@
-import {
-  RadixNetwork,
-  RadixNetworkConfig,
-} from '@radixdlt/babylon-gateway-api-sdk'
+import { RadixNetworkConfig } from '@radixdlt/babylon-gateway-api-sdk'
 
-const normalizeBasePath = (basePath?: string) => {
-  if (!basePath) return ''
-  return basePath.endsWith('/') ? basePath?.slice(0, -1) : basePath
-}
-
-export const networkIdMap = new Map<number, string>()
-  .set(
-    RadixNetwork.RCnetV1,
-    normalizeBasePath(RadixNetworkConfig.RCnetV1?.gatewayUrl || '')
-  )
-  .set(
-    RadixNetwork.RCnetV2,
-    normalizeBasePath(RadixNetworkConfig.RCnetV2?.gatewayUrl || '')
-  )
-  .set(
-    RadixNetwork.Enkinet,
-    normalizeBasePath(RadixNetworkConfig.Enkinet?.gatewayUrl || '')
-  )
-  .set(
-    RadixNetwork.Hammunet,
-    normalizeBasePath(RadixNetworkConfig.Hammunet?.gatewayUrl || '')
-  )
+export const networkIdMap = new Map<number, string>(
+  Object.values(RadixNetworkConfig).map(({ networkId, gatewayUrl }) => [
+    networkId,
+    gatewayUrl,
+  ])
+)
