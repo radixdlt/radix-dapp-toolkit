cf60bdb036d0a4656db22bc7a9f85d7dc47560ec
Alex Stelea <alex.s@rdx.works>
2023-07-20 17:36:50 +0200

    fix(sandbox): secp256k1 rola verification

--- examples/rola/helpers/verify-proof.ts
+++ examples/rola/helpers/verify-proof.ts
@@ -1,7 +1,9 @@
 import { Result, err, ok } from 'neverthrow'
 import { curve25519 } from '../crypto/curve25519'
-import { SignedChallenge } from '../../../src/io/schemas'
 import { secp256k1 } from '../crypto/secp256k1'
+import { ec, BNInput, SignatureInput } from 'elliptic'
+import { SignedChallenge } from '../../../src'
+import { Buffer } from 'buffer'
 
 const supportedCurves = new Set(['curve25519', 'secp256k1'])
 
@@ -14,16 +16,25 @@ export const verifyProofFactory =
     if (!isSupportedCurve) return err({ reason: 'unsupportedCurve' })
 
     try {
-      const publicKey =
-        input.proof.curve === 'curve25519'
-          ? // @ts-ignore: incorrect type definition in EC lib
-            curve25519.keyFromPublic(input.proof.publicKey, 'hex')
-          : secp256k1.keyFromPublic(input.proof.publicKey, 'hex')
+      let isValid = false
 
-      const isValid = publicKey.verify(
-        signatureMessageHex,
-        input.proof.signature
-      )
+      if (input.proof.curve === 'curve25519') {
+        const publicKey = curve25519.keyFromPublic(
+          input.proof.publicKey,
+          // @ts-ignore: incorrect type definition in EC lib
+          'hex'
+        )
+        isValid = publicKey.verify(signatureMessageHex, input.proof.signature)
+      } else {
+        const signature = Buffer.from(input.proof.signature, 'hex')
+          .toJSON()
+          .data.slice(1)
+        const r = signature.slice(0, 32)
+        const s = signature.slice(32, 64)
+        isValid = secp256k1
+          .keyFromPublic(input.proof.publicKey, 'hex')
+          .verify(signatureMessageHex, { r, s })
+      }
       return isValid ? ok(undefined) : err({ reason: 'invalidSignature' })
     } catch (error: any) {
       return err({ reason: 'invalidPublicKey', jsError: error })
