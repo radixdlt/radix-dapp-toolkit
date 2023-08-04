3db1737e4d5814d9b94eb24eed76eaaccd2a1a90
Dawid Sowa <dawid.sowa@rdx.works>
2023-08-04 17:04:57 +0200

    feat(sandbox): add CB config in sandbox settings

--- examples/rdt/rdt.ts
+++ examples/rdt/rdt.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from 'rxjs'
+import { BehaviorSubject, tap } from 'rxjs'
 import {
   DataRequestBuilder,
   DataRequestStateClient,
@@ -19,6 +19,7 @@ import {
   RadixNetwork,
   RadixNetworkConfigById,
 } from '@radixdlt/babylon-gateway-api-sdk'
+import { connectButtonConfigSubject } from './state'
 
 const networkId = networkIdSubject.value
 
@@ -113,3 +114,12 @@ rdt.walletApi.setRequestData(
     accounts: { numberOfAccounts: { quantifier: 'atLeast', quantity: 1 } },
   })
 )
+
+connectButtonConfigSubject
+  .pipe(
+    tap((value) => {
+      rdt.buttonApi.setTheme(value.theme as any)
+      rdt.buttonApi.setMode(value.mode as any)
+    })
+  )
+  .subscribe()
