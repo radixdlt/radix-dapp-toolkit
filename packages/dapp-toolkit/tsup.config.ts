0ce4955e22610ca0e6163556195a24f222de2aeb
Dawid Sowa <dawid.sowa@rdx.works>
2024-05-07 13:13:38 +0200

    feat: add connect-button entry point

--- packages/dapp-toolkit/tsup.config.ts
+++ packages/dapp-toolkit/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/connect-button.ts'],
   dts: true,
   format: ['esm', 'cjs'],
   noExternal: ['@radixdlt/connect-button', 'radix-connect-common'],
