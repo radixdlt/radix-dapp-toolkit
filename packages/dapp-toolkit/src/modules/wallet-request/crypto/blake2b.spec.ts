c9fd3d09fe372a853a8f2e4eb2a3eee31c54c874
Alex Stelea <alex.s@rdx.works>
2024-06-14 15:59:46 +0100

    test: add unit test

--- packages/dapp-toolkit/src/modules/wallet-request/crypto/blake2b.spec.ts
+++ packages/dapp-toolkit/src/modules/wallet-request/crypto/blake2b.spec.ts
@@ -11,4 +11,14 @@ describe('blake2b', () => {
       '928b20366943e2afd11ebc0eae2e53a93bf177a4fcf35bcc64d503704e65e202',
     )
   })
+
+  it('should generate an array of blake2b hashes', async () => {
+    const vectors = new Array(100).fill(null).map(() => {
+      const buffer = Buffer.from(crypto.getRandomValues(new Uint8Array(64)))
+      const blake2bHash = blake2b(buffer)._unsafeUnwrap().toString('hex')
+      const message = buffer.toString('hex')
+      return { message, blake2bHash }
+    })
+    console.log(vectors)
+  })
 })
