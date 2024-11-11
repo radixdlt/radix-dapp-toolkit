db3fbf2e86bf32e2ded18b22d19d989f92e35d85
Alex Stelea <alex.s@rdx.works>
2024-11-11 18:48:10 +0000

    code(rdt): update single file bundle extension

--- packages/dapp-toolkit/vite-single-file.config.ts
+++ packages/dapp-toolkit/vite-single-file.config.ts
@@ -3,12 +3,20 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   plugins: [viteSingleFile()],
+
   build: {
     emptyOutDir: false,
+
     lib: {
       entry: 'src/single-file.js',
       name: 'RDT',
       fileName: 'radix-dapp-toolkit.bundle',
+      formats: ['umd'],
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: `radix-dapp-toolkit.bundle.umd.js`,
+      },
     },
   },
   define: { 'process.env.NODE_ENV': '"production"' },
