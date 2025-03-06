d9604fc8d1741dea5813a71c1a50cbfad00b71c6
Alex Stelea <alex.s@rdx.works>
2025-03-06 15:20:23 +0000

    build: migrate to pnpm

--- packages/dapp-toolkit/vite-single-file.config.ts
+++ packages/dapp-toolkit/vite-single-file.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
+  // @ts-expect-error
   plugins: [viteSingleFile()],
 
   build: {
